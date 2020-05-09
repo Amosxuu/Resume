a7010c24a1a8cb112c864699fa593d1e011278f1
zxceve266 <zxceve266@gmail.com>
2020-05-09 20:30:39 +0800

    banner補全新增特效

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
-// let slides = document.querySelectorAll('.works-img img')
-// let slidesText = document.querySelectorAll('.banner-text div')
-// let prevBtn = document.querySelector('.controls .prev');
-// let nextBtn = document.querySelector('.controls .next');
+
 let menu = document.querySelector('.menu-icon');
 let index = 0;
 
-// function nextSlide(e) {
-//     e.preventDefault()
-//     slidesText[index].classList.remove('active');
-//     slides[index].classList.remove('active');
-//     index =(index + 1) % slides.length;
-//     slidesText[index].classList.add('active');
-//     slides[index].classList.add('active');
-// }
-// function prevSlide(e) {
-//     e.preventDefault()
-//     slidesText[index].classList.remove('active');
-//     slides[index].classList.remove('active');
-//     index = (index - 1 + slides.length) % slides.length;
-//     slidesText[index].classList.add('active');
-//     slides[index].classList.add('active');
-// }
+window.addEventListener('scroll',()=>{
+
+    let banner = document.querySelector('.banner-img');
+
+    banner.style.opacity = 1 - window.pageYOffset/550;
+    console.log(window.pageYOffset)
+    banner.style.top = +window.pageYOffset +'px';
+    banner.style.backgroundPositionY = - window.pageYOffset/2 +'px'
+
+   
+})
 
 function meunToggle(){
     let navbar = document.querySelector('#navbar');
     navbar.classList.toggle('active')
 }
-
-// nextBtn.addEventListener('click',nextSlide);
-// prevBtn.addEventListener('click',prevSlide);
 menu.addEventListener('click',meunToggle)
