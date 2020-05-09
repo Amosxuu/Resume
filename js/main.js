b9a387e76b13fea4f973d8d2e288e79645ebe70b
zxceve266 <zxceve266@gmail.com>
2020-05-09 23:35:17 +0800

    about 製作部分 修改Banner

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
 let menu = document.querySelector('.menu-icon');
 let index = 0;
 
-window.addEventListener('scroll',()=>{
+// window.addEventListener('scroll',()=>{
 
-    let banner = document.querySelector('.banner-img');
+//     let banner = document.querySelector('.banner-img');
 
-    banner.style.opacity = 1 - window.pageYOffset/550;
-    console.log(window.pageYOffset)
-    banner.style.top = +window.pageYOffset +'px';
-    banner.style.backgroundPositionY = - window.pageYOffset/2 +'px'
+//     banner.style.opacity = 1 - window.pageYOffset/550;
+//     console.log(window.pageYOffset)
+//     banner.style.top = +window.pageYOffset +'px';
+//     banner.style.backgroundPositionY = - window.pageYOffset/2 +'px'
 
    
-})
+// })
 
 function meunToggle(){
     let navbar = document.querySelector('#navbar');
